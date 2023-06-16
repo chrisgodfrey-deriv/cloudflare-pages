8ffd2a5ecbd31d5a8abc70ef4a22e2e07c9ed7bb
Chris Godfrey <chris.godfrey@deriv.com>
2023-06-16 08:40:03 +0100

    Refactor of the middleware script to be more readable

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,39 +1,42 @@
 const cookieName = "ab-test-cookie"
-const newHomepagePathName = "/ver2" // b url
+const newHomepagePathName = "/ver2"
 const throttle = 50 // 50% of traffic 
 
 const abTest = async ({ request, next, env }) => {
   const url = new URL(request.url)
-  // if homepage
-  if (url.pathname === "/") {
 
-    let cookie = request.headers.get("cookie")
-    // is cookie set and is new cohort?
-    if (cookie) {
-      if(cookie.includes(`${cookieName}=new`)) {
-        // pass the request to /test
-        url.pathname = newHomepagePathName
-        return env.ASSETS.fetch(url)
-      } else {
-        return env.ASSETS.fetch(url)
-      }
-    } else {
-      const percentage = Math.floor(Math.random() * 100)
-      let version = "current" // default version
-      
-      // change pathname and version name for 
-      if (percentage < throttle) {
-        url.pathname = newHomepagePathName
-        version = "new"
-      }
-      // get the static file from ASSETS, and attach a cookie
-      const asset = await env.ASSETS.fetch(url)
-      let response = new Response(asset.body, asset)
-      response.headers.append("Set-Cookie", `${cookieName}=${version}; path=/`)
-      return response
-    }
+  // url is not on the homepage
+  if (url.pathname !== "/") {
+    return next();
   }
-  return next()
+
+  const cookie = request.headers.get("cookie")
+
+  // client is in the `new` test
+  if (cookie && cookie.includes(`${cookieName}=new`)) {
+    url.pathname = newHomepagePathName
+    return env.ASSETS.fetch(url)
+  }
+
+  // client is in the `old` test
+  if (cookie && cookie.includes(`${cookieName}=current`)) {
+    return next()
+  }
+
+  // client is not in a test
+  const percentage = Math.floor(Math.random() * 100)
+  const version = percentage < throttle ? 'new' : 'current';
+
+  if (version === 'new') {
+    url.pathname = newHomepagePathName
+  }
+
+  const asset = await env.ASSETS.fetch(url)
+  const response = new Response(asset.body, asset)
+
+  response.headers.append("Set-Cookie", `${cookieName}=${version}; path=/`)
+
+  return response
 };
 
 export const onRequest = [abTest];
